f9972f3bcf7bcbe852dcaa8ef11eb80a59cc985c
Sooyoung Yoon <ayoooo240@gmail.com>
2022-08-09 15:31:59 +0900

    💄 글로벌 스타일 추가
    Co-authored-by: slozche <slozche@users.noreply.github.com>
    Co-authored-by: doa12 <doa12@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
-function App() {
-  return <div className="App">
-    <p>안녕하세요~</p>
-  </div>;
-}
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import GlobalStyle from './styles/GlobalStyle';
+
+const Home = React.lazy(() => import('./pages/Home'));
+const Login = React.lazy(() => import('./pages/login'));
+const Signup = React.lazy(() => import('./pages/signup'));
+const Calender = React.lazy(() => import('./pages/calender'));
+const Gallery = React.lazy(() => import('./pages/gallery'));
+const Voicemail = React.lazy(() => import('./pages/voicemail'));
+const Community = React.lazy(() => import('./pages/community'));
+const Ranking = React.lazy(() => import('./pages/ranking'));
+
+const App = () => {
+  return (
+    <>
+      <GlobalStyle />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/schedules" element={<Calender />} />
+        <Route path="/galleries" element={<Gallery />} />
+        <Route path="/voice-mails" element={<Voicemail />} />
+        <Route path="/community" element={<Community />} />
+        <Route path="/rankings" element={<Ranking />} />
+      </Routes>
+    </>
+  );
+};
 
 export default App;
---
+++ src/styles/GlobalStyle.jsx
@@ -0,0 +1,51 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+    /*! minireset.css v0.0.6 | MIT License | github.com/jgthms/minireset.css */
+    *, :after, :before {
+        box-sizing:inherit;
+    }
+    html {
+        box-sizing:border-box;
+    }
+    blockquote, body, dd, dl, dt, fieldset, figure, h1, h2, h3, h4, h5, h6, hr, html, iframe, legend, li, ol, p, pre, textarea, ul {
+        margin: 0;
+        padding: 0;
+    }
+    h1, h2, h3, h4, h5, h6 {
+        font-size: 100%;
+        font-weight: 400;
+    }
+    ul {
+        list-style: none;
+    }
+    button, input, select {
+        margin: 0;
+        border: none;
+        background: none;
+        outline: none;
+    }
+    button {
+        cursor: pointer;
+    }
+    img, video {
+        height: auto;
+        max-width: 100%;
+    }
+    iframe {
+        border: 0;
+    }
+    table {
+        border-collapse: collapse;
+        border-spacing: 0;
+    }
+    td, th {
+        padding: 0;
+    }
+    a {
+        color: inherit;
+        text-decoration: none;
+    }
+`;
+
+export default GlobalStyle;
