f792040bb34bd7ae105b136e4e24ed4bc183a250
Sooyoung Yoon <ayoooo240@gmail.com>
2022-08-14 12:09:28 +0900

    ✨ calendarSlice 생성 및 컴포넌트에 store 공급

--- src/index.js
+++ src/index.js
@@ -5,16 +5,20 @@ import { HelmetProvider } from 'react-helmet-async';
 import { BrowserRouter } from 'react-router-dom';
 import Loading from './components/common/Loading';
 import AuthProvider from './context/AuthProvider';
+import { Provider } from 'react-redux';
+import store from './redux/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Suspense fallback={<Loading />}>
-    <AuthProvider>
-      <BrowserRouter>
-        <HelmetProvider>
-          <App />
-        </HelmetProvider>
-      </BrowserRouter>
-    </AuthProvider>
+    <Provider store={store}>
+      <AuthProvider>
+        <BrowserRouter>
+          <HelmetProvider>
+            <App />
+          </HelmetProvider>
+        </BrowserRouter>
+      </AuthProvider>
+    </Provider>
   </Suspense>,
 );
