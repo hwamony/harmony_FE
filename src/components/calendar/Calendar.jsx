eb16fdb4ce5148305a7897a56b37077437e5f677
Sooyoung Yoon <ayoooo240@gmail.com>
2022-08-14 12:11:22 +0900

    ✨ 이번 달을 나타내는 캘린더 컴포넌트 구현

--- src/components/calendar/Calendar.jsx
+++ src/components/calendar/Calendar.jsx
@@ -1,7 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import { getMonth } from '../../util';
+import { useDispatch, useSelector } from 'react-redux';
+import { setMonthIdx } from '../../redux/modules/calendarSlice';
+import Month from './Month';
 
 const Calendar = () => {
-  return <div>Calendar</div>;
+  const dispatch = useDispatch();
+  const { monthIdx } = useSelector((state) => state.calendar);
+  const [curMonth, setCurMonth] = useState(getMonth());
+
+  return (
+    <CalendarContainer>
+      <h2>{monthIdx + 1}월</h2>
+      <Month monthMatrix={curMonth} monthIdx={monthIdx} />
+    </CalendarContainer>
+  );
 };
 
 export default Calendar;
+
+const CalendarContainer = styled.article`
+  display: flex;
+  flex-direction: column;
+  height: 356px;
+  text-align: center;
+  h2 {
+    margin: 1em 0;
+    font-size: 1.2em;
+    font-weight: 700;
+  }
+`;
