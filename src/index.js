e37c457bf204c8d51912d8c14e1ac5bf0806db10
Sooyoung Yoon <ayoooo240@gmail.com>
2022-08-15 18:14:53 +0900

    ➕ @tanstack/react-query 추가

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
 import Loading from './components/common/Loading';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Provider } from 'react-redux';
 import store from './redux/store';
 import AuthProvider from './context/AuthProvider';
@@ -10,19 +11,29 @@ import { ThemeProvider } from '@mui/material/styles';
 import { theme } from './styles/GlobalStyle';
 import App from './App';
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      suspense: true,
+    },
+  },
+});
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Suspense fallback={<Loading />}>
-    <Provider store={store}>
-      <AuthProvider>
-        <BrowserRouter>
-          <HelmetProvider>
-            <ThemeProvider theme={theme}>
-              <App />
-            </ThemeProvider>
-          </HelmetProvider>
-        </BrowserRouter>
-      </AuthProvider>
-    </Provider>
+    <QueryClientProvider client={queryClient}>
+      <Provider store={store}>
+        <AuthProvider>
+          <BrowserRouter>
+            <HelmetProvider>
+              <ThemeProvider theme={theme}>
+                <App />
+              </ThemeProvider>
+            </HelmetProvider>
+          </BrowserRouter>
+        </AuthProvider>
+      </Provider>
+    </QueryClientProvider>
   </Suspense>,
 );
