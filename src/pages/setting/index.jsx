1b41dee1cccf56d171d97d0ff777b04a59d0ee1a
Sooyoung Yoon <ayoooo240@gmail.com>
2022-08-24 05:22:45 +0900

    ✨ 설정 페이지 구현

--- src/pages/setting/index.jsx
+++ src/pages/setting/index.jsx
@@ -2,40 +2,76 @@ import React from 'react';
 import PageTitle from '../../components/common/PageTitle';
 import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
-import { Button } from '@mui/material';
+import Widget from '../../components/family/Widget';
+import { IconNext } from '../../assets/icons';
 
 const Setting = () => {
   const navigate = useNavigate();
   return (
     <>
       <PageTitle title="설정" />
-      <div>
-        <BtnAuth
-          variant="contained"
+      <Widget />
+      <SettingsContainer>
+        <h3>초대코드</h3>
+        <div className="box-code">
+          {/* TODO: 가족코드 조회 API 요청 */}
+          <p>94321114</p>
+        </div>
+        <button type="button" onClick={() => navigate('/rankings')}>
+          <h3>가족랭킹</h3>
+          <IconNext />
+        </button>
+        <button
           onClick={() => {
             localStorage.removeItem('TOKEN');
             window.location.href = '/login';
           }}
         >
-          로그아웃 (토큰 삭제)
-        </BtnAuth>
-
-        <BtnAuth variant="outlined" onClick={() => navigate('/login')}>
-          <p>로그인 링크 (로그인 시 진입 X)</p>
-        </BtnAuth>
-
-        <BtnAuth variant="outlined" onClick={() => navigate('/signup')}>
-          <p>회원가입 링크 (로그인 시 진입 X)</p>
-        </BtnAuth>
-      </div>
+          <h3>로그아웃</h3>
+          <IconNext />
+        </button>
+      </SettingsContainer>
     </>
   );
 };
 
 export default Setting;
 
-const BtnAuth = styled(Button)`
-  display: block;
-  margin: 20px auto;
-  border-radius: 30px;
+const SettingsContainer = styled.section`
+  position: relative;
+  overflow-y: auto;
+  height: calc(100vh - 75px - 65px);
+  margin-top: 119px;
+  padding: 0px 20px 25px;
+
+  h3 {
+    display: inline-block;
+    font-size: 14px;
+    font-weight: 700;
+  }
+
+  div.box-code {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 66px;
+    margin: 10px 0 19px;
+    border-radius: 5px;
+    background: #eee;
+    font-size: 20px;
+    font-weight: 700;
+    p {
+      border-bottom: 2px solid #000;
+    }
+  }
+
+  button {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 100%;
+    margin-bottom: 5px;
+    padding: 24px 0;
+    border-bottom: 1px solid #dadada;
+  }
 `;
