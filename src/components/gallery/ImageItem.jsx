d22bde48c65ffd02ed3c3095ff5441bda36744ae
Sooyoung Yoon <ayoooo240@gmail.com>
2022-08-25 04:16:31 +0900

    ✨ 앨범 페이지 이미지 선택 기능 및 조건부 footer 구현

---
+++ src/components/gallery/ImageItem.jsx
@@ -0,0 +1,55 @@
+import React, { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
+import { useSelector } from 'react-redux';
+import { IconCheck } from '../../assets/icons';
+
+const ImageItem = ({ url, handleCheck }) => {
+  const { onSelect } = useSelector((state) => state.gallery);
+  const [checked, setChecked] = useState(false);
+
+  useEffect(() => {
+    setChecked(false);
+  }, [onSelect]);
+
+  const handleItemCheck = ({ target }) => {
+    setChecked(!checked);
+    // FIXME: url을 img.imageId로 수정하기
+    handleCheck(url, target.checked);
+  };
+
+  return (
+    <div>
+      {onSelect && (
+        <>
+          <input
+            type="checkbox"
+            name="check-img"
+            id={url}
+            value={url}
+            checked={checked}
+            onChange={(e) => handleItemCheck(e)}
+            hidden
+          />
+          <label htmlFor={url} className="label-check">
+            <IconCheck />
+            <span className="hidden">사진 선택</span>
+          </label>
+        </>
+      )}
+      {onSelect ? (
+        <label htmlFor={url}>
+          <img src={url} alt="" />
+        </label>
+      ) : (
+        <img src={url} alt="" />
+      )}
+    </div>
+  );
+};
+
+ImageItem.propTypes = {
+  url: PropTypes.string,
+  handleCheck: PropTypes.func,
+};
+
+export default ImageItem;
--- src/pages/gallery/Album.jsx
+++ src/pages/gallery/Album.jsx
@@ -1,16 +1,20 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams, Link, useLocation } from 'react-router-dom';
 import styled from 'styled-components';
 import cn from 'classnames';
 import { useSelector, useDispatch } from 'react-redux';
 import { setOnSelect, setOnSelectAll } from '../../redux/modules/gallerySlice';
 import HeaderMid from '../../components/common/HeaderMid';
-import { IconCheck } from '../../assets/icons';
+import { IconSave } from '../../assets/icons';
+import { FiTrash2 } from 'react-icons/fi';
+import ImageItem from '../../components/gallery/ImageItem';
 
 const Album = () => {
   const params = useParams();
   const dispatch = useDispatch();
   const { onSelect, onSelectAll } = useSelector((state) => state.gallery);
+  const [checkedImgs, setCheckedImgs] = useState(new Set());
+  const [size, setSize] = useState(0);
   // FIXME: API 요청할 때는 캐싱된 데이터 사용하기
   const location = useLocation();
   const albumsData = location.state;
@@ -22,7 +26,24 @@ const Album = () => {
     };
   }, []);
 
-  // TODO: 이미지id를 checkList 배열에 담기
+  useEffect(() => {
+    // console.log([...checkedImgs]);
+    setCheckedImgs(new Set());
+    setSize(0);
+  }, [onSelect]);
+
+  const handleCheck = (id, isChecked) => {
+    if (isChecked) {
+      checkedImgs.add(id);
+      setCheckedImgs(checkedImgs);
+      setSize(checkedImgs.size);
+    } else if (!isChecked && checkedImgs.has(id)) {
+      checkedImgs.delete(id);
+      setCheckedImgs(checkedImgs);
+      setSize(checkedImgs.size);
+    }
+  };
+
   // TODO: 전체 선택 기능 구현하기
 
   return (
@@ -47,38 +68,23 @@ const Album = () => {
               </li>
             ))}
           </AlbumList>
+
           <ImageList>
             {albumsData.albums
               .filter((v) => v.albumId === parseInt(params.albumId))[0]
-              .images.map((url, i) => (
-                <div key={i}>
-                  {onSelect && (
-                    <>
-                      <input
-                        type="checkbox"
-                        name="check-img"
-                        id={url}
-                        value={url}
-                        hidden
-                      />
-                      <label htmlFor={url} className="label-check">
-                        <IconCheck />
-                        <span className="hidden">사진 선택</span>
-                      </label>
-                    </>
-                  )}
-                  {onSelect ? (
-                    <label htmlFor={url}>
-                      <img src={url} alt="" />
-                    </label>
-                  ) : (
-                    <img src={url} alt="" />
-                  )}
-                </div>
+              .images.map((img) => (
+                // FIXME: key와 url을 img.imageId로 수정하기
+                <ImageItem key={img} url={img} handleCheck={handleCheck} />
               ))}
           </ImageList>
         </>
       )}
+
+      <SelectFooter className={cn(onSelect && 'on')}>
+        <IconSave />
+        <strong>선택 {size}</strong>
+        <FiTrash2 />
+      </SelectFooter>
     </AlbumSection>
   );
 };
@@ -144,5 +150,36 @@ const ImageList = styled.div`
   }
   input:checked + label.label-check {
     background: ${({ theme }) => theme.palette.primary.main};
+  }
+`;
+
+const SelectFooter = styled.footer`
+  position: fixed;
+  left: 0;
+  right: 0;
+  bottom: -64px;
+  display: flex;
+  justify-content: space-between;
+  height: 64px;
+  padding: 0 17px;
+  background: #fff;
+  color: #868686;
+  font-size: 12px;
+  transition: all 0.2s ease-in-out;
+  z-index: 150;
+  &.on {
+    bottom: 0;
+  }
+  strong {
+    margin-top: 1em;
+    color: #18191f;
+    font-size: 14px;
+    font-weight: 700;
+  }
+  svg {
+    width: 24px;
+    height: 24px;
+    margin-top: 10px;
+    color: ${({ theme }) => theme.palette.primary.main};
   }
 `;
