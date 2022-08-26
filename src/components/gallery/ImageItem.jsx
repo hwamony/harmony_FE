e066c01de94fc93c5cf05a3617cb683a1d9f6f44
Sooyoung Yoon <ayoooo240@gmail.com>
2022-08-27 05:11:12 +0900

    ✨ 앨범 및 이미지 상세 페이지 조회 기능 구현

--- src/components/gallery/ImageItem.jsx
+++ src/components/gallery/ImageItem.jsx
@@ -1,10 +1,9 @@
 import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { useSelector } from 'react-redux';
 import { IconCheck } from '../../assets/icons';
 
-const ImageItem = ({ url, handleCheck }) => {
+const ImageItem = ({ img, handleCheck }) => {
   const { onSelect } = useSelector((state) => state.gallery);
   const [checked, setChecked] = useState(false);
 
@@ -14,44 +13,42 @@ const ImageItem = ({ url, handleCheck }) => {
 
   const handleItemCheck = ({ target }) => {
     setChecked(!checked);
-    // FIXME: url을 img.imageId로 수정하기
-    handleCheck(url, target.checked);
+    handleCheck(img.id, target.checked);
   };
 
   return (
-    // FIXME: imageId로 수정하기
     <div>
       {onSelect && (
         <>
           <input
             type="checkbox"
             name="check-img"
-            id={url}
-            value={url}
+            id={img.id}
+            value={img.id}
             checked={checked}
             onChange={(e) => handleItemCheck(e)}
             hidden
           />
-          <label htmlFor={url} className="label-check">
+          <label htmlFor={img.id} className="label-check">
             <IconCheck />
             <span className="hidden">사진 선택</span>
           </label>
         </>
       )}
       {onSelect ? (
-        <label htmlFor={url}>
-          <img src={url} alt="" />
+        <label htmlFor={img.id}>
+          <img src={img.url} alt="" />
         </label>
       ) : (
-        <img src={url} alt="" />
+        <img src={img.url} alt="" />
       )}
     </div>
   );
 };
 
 ImageItem.propTypes = {
-  url: PropTypes.string,
-  handleCheck: PropTypes.func,
+  img: PropTypes.object.isRequired,
+  handleCheck: PropTypes.func.isRequired,
 };
 
 export default ImageItem;
