ad06cf63ceb2a47f85334af1160c1eb441554bda
Sooyoung Yoon <ayoooo240@gmail.com>
2022-09-01 04:42:13 +0900

    ✨ 커뮤니티 글 조회 API 연결

--- src/components/community/ShortCard.jsx
+++ src/components/community/ShortCard.jsx
@@ -1,73 +1,78 @@
-import React, { useState, useMemo, useRef, useEffect } from 'react';
+import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import PropTypes from 'prop-types';
 import styled from 'styled-components';
-import { FaRegComment, FaRegThumbsUp } from 'react-icons/fa'
-import { RiPlantLine } from 'react-icons/ri';
+import { FaRegComment, FaRegThumbsUp } from 'react-icons/fa';
 
-
-const shortCard = () => {
-  const [like, setLike] = useState(0);
-  // const [isShowMore, setIsShowMore] = useState<boolean>(false);
-  // const textLimit = useRef<number>(246);
-  
-  // const commenter = useMemo(() => {
-  //   const shortReview: string = comment.slice(0, textLimit.current);
-    
-  //   if (comment.length > textLimit.current) {
-  //     if (isShowMore) { return comment; }
-  //     return showrtReview
-  //   }
-  //   return comment;
-  // }, [isShowMore]);
+const ShortCard = ({ post }) => {
   const navigate = useNavigate();
+
   return (
     <>
       <CardContainer>
         <Inventory>
           <CardTitle>
             <Profile>
-              <Photo>프사</Photo>
+              <div>{post.poster.flower}꽃여부</div>
+              <Photo>{post.poster.level}화목등급</Photo>
               <Info>
-                <h5><span><RiPlantLine/></span> 화목화목</h5>
-                <p>12시간 전</p>
-                </Info>
+                <h5>{post.poster.nickname}</h5>
+                <p>{post.createdAt}</p>
+              </Info>
             </Profile>
-            <p>. . .</p>
+            <p>. . .
+              {/* TODO: MoreVert 추가 (수정,삭제메뉴) */}
+            </p>
           </CardTitle>
+
           <CardContent>
             <H1>
-              <h1>제목입니다</h1>
+              <h1>{post.title}</h1>
               <H3>
-                <p> <FaRegThumbsUp /> {like}</p>
-                <p> <FaRegComment /> 322</p>
+                <p>
+                  <FaRegThumbsUp /> {post.likeCount}
+                </p>
+                <p>
+                  <FaRegComment /> {post.commentCount}
+                </p>
               </H3>
             </H1>
             <H1>
               <Image>
-                <div/>
-                <h3 onClick={() => navigate('/posts/comments')}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut quam risus, convallis sit amet justo in, venenatis lacinia dui. Suspendisse potenti. Vivamus quis risus eleifend, eleifend augue in, ultricies nisl. Phasellus at fermentum nibh. Cras fin
-                  <span >... 더 보기</span>
-                  {/* <span >... { commenter }</span> */}
+                <img src={post.imageUrl} alt="" />
+                <h3 onClick={() => navigate(`/community/posts/${post.postId}`)}>
+                  {post.content}
+                  <span>... 더 보기</span>
                 </h3>
               </Image>
             </H1>
             <Tag>
-              <h3>#일상</h3>
-              <h3>#고민</h3>
-              <h3>#아무말</h3>
-              <h3>#이거왜검색할때마다달라</h3>
+              {post.tags.map((tag) => (
+                <h3 key={tag}>#{tag}</h3>
+              ))}
             </Tag>
           </CardContent>
+
           <CardGood>
-            <h5 onClick={()=> {setLike(like + 1)} } ><FaRegThumbsUp/> 좋아요</h5>
-            <h5 onClick={() => navigate('/posts/comments')}><FaRegComment/> 댓글달기</h5>
+            <h5>
+              {/* TODO: 좋아요 api 추가 */}
+              <FaRegThumbsUp /> 좋아요
+            </h5>
+            <h5 onClick={() => navigate(`/community/posts/${post.postId}`)}>
+              <FaRegComment /> 댓글
+            </h5>
           </CardGood>
         </Inventory>
-    </CardContainer>
+      </CardContainer>
     </>
-  )
-}
-export default shortCard;
+  );
+};
+
+ShortCard.propTypes = {
+  post: PropTypes.object.isRequired,
+};
+
+export default ShortCard;
 
 export const CardContainer = styled.div`
   padding-top: 3vh;
@@ -88,50 +93,50 @@ export const Inventory = styled.div`
   border-radius: 5px;
   text-align: center;
   background-color: white;
-`
+`;
 
 export const CardTitle = styled.div`
-  border-bottom: 0.01px solid #D9D9D9;
+  border-bottom: 0.01px solid #d9d9d9;
   padding-right: 20px;
   display: flex;
   flex-direction: row;
   align-items: center;
   justify-content: space-between;
   height: 75px;
-  width: 670px;
-  p{
+  width: 100%;
+  p {
     color: gray;
     font-size: 13px;
     font-weight: bolder;
-    :hover{
+    :hover {
       cursor: pointer;
     }
   }
-`
+`;
 
 export const Profile = styled.div`
   display: flex;
   flex-direction: row;
   justify-content: left;
   align-items: center;
-`
+`;
 
 export const Photo = styled.p`
-margin: 0 15px;
-padding: 15px 13px;
-background-color: #D9D9D9;
-border-radius: 100%;
-`
+  margin: 0 15px;
+  padding: 15px 13px;
+  background-color: #d9d9d9;
+  border-radius: 100%;
+`;
 
 export const Info = styled.div`
   flex-direction: column;
-  span{
-    color: #3EC192;
+  span {
+    color: #3ec192;
   }
-`
+`;
 
 export const CardContent = styled.div`
-  width: 660px;
+  width: 100%;
   height: 200px;
   display: flex;
   flex-direction: column;
@@ -139,17 +144,17 @@ export const CardContent = styled.div`
   h3 {
     margin: 5px 10px;
     text-align: left;
-  };
-  span{
+  }
+  span {
     color: gray;
     font-size: 13px;
-    :hover{
+    :hover {
       cursor: pointer;
     }
-  };
-`
+  }
+`;
 export const H1 = styled.div`
-  width: 690px;
+  width: 100%;
   display: flex;
   flex-direction: row;
   justify-content: space-between;
@@ -160,11 +165,11 @@ export const H1 = styled.div`
     font-size: 1.2em;
     font-weight: 700;
     text-align: left;
-  };
-  h3{
-    width: 550px;
   }
-`
+  h3 {
+    width: 100%;
+  }
+`;
 
 export const H3 = styled.div`
   width: 180px;
@@ -172,11 +177,11 @@ export const H3 = styled.div`
   flex-direction: row;
   justify-content: space-around;
   align-items: center;
-  p{
+  p {
     padding-right: 50px;
     // color: #3EC192;
   }
-`
+`;
 
 export const Image = styled.div`
   display: flex;
@@ -184,66 +189,65 @@ export const Image = styled.div`
   justify-content: left;
   align-items: center;
   margin-bottom: 7.5px;
-  div{
-    background-image: url('https://t1.daumcdn.net/cfile/blog/21150B4654FFDC532D');
+  img {
     width: 90px;
     height: 75px;
     margin-left: 25px;
   }
-  h3{
-    :hover{
+  h3 {
+    :hover {
       cursor: pointer;
     }
   }
-`
+`;
 
 export const Tag = styled.div`
-  width: 690px;
+  width: 100%;
   height: 20px;
   padding-left: 10px;
   display: flex;
   flex-direction: row;
   justify-content: left;
   align-items: center;
-  h3{
+  h3 {
     margin: 0 5px;
     padding: 2px 10px;
     margin-bottom: 10px;
     border-radius: 10px;
-    background-color: #E7E7E7;
+    background-color: #e7e7e7;
     color: gray;
     font-size: 13px;
     text-align: left;
   }
-`
+`;
 
 export const GoodCount = styled.div`
-  width: 100vw;
+  width: 100%;
   height: 3vh;
   padding-left: 20px;
   display: flex;
   flex-direction: row;
   justify-content: left;
   align-items: center;
-  p{
-    color: #9E9E9E;
+  p {
+    color: #9e9e9e;
     padding-right: 10px;
     font-size: 13px;
   }
-`
+`;
 
 export const CardGood = styled.div`
-  border-top: 0.01px solid #D9D9D9;
+  border-top: 0.01px solid #d9d9d9;
   display: flex;
   flex-direction: row;
   align-items: center;
   width: 100%;
   height: 45px;
   font-size: 15px;
-  h5{
+  h5 {
     padding: 0 135px;
-      :hover{
+    :hover {
       cursor: pointer;
     }
   }
-`
+`;
--- src/pages/community/index.jsx
+++ src/pages/community/index.jsx
@@ -1,5 +1,9 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { useInfiniteQuery } from '@tanstack/react-query';
+import { useInView } from 'react-intersection-observer';
+import api from '../../api/AxiosManager';
+
 import PageTitle from '../../components/common/PageTitle';
 import {
   CommunityColor,
@@ -8,26 +12,54 @@ import {
   Category,
   Order,
 } from './style';
-import { communityRoles } from '../../utils/data';
 import ShortCard from '../../components/community/ShortCard';
 import Header from '../../components/common/Header';
 import BtnAdd from '../../components/common/BtnAdd';
+import { communityRoles } from '../../utils/data';
+import Loading from '../../components/common/Loading';
 
 const Community = () => {
   const navigate = useNavigate();
+  // const [category, setCategory] = useState();
+  const { ref, inView } = useInView();
+
+  const getCommunityPosts = async (pageParam = 0) => {
+    // FIXME: 카테고리 수정하기
+    const res = await api.get(`/posts?category=전체&page=${pageParam}&size=10`);
+    const data = res.data.data.content;
+    const last = res.data.data.last;
+    return { data, last, nextPage: pageParam + 1 };
+  };
+
+  const {
+    data: postList,
+    fetchNextPage,
+    isFetchingNextPage,
+  } = useInfiniteQuery(
+    ['communityPosts'],
+    ({ pageParam = 0 }) => getCommunityPosts(pageParam),
+    {
+      refetchOnWindowFocus: false,
+      getNextPageParam: (lastPage) =>
+        !lastPage.last ? lastPage.nextPage : undefined,
+      onSuccess: (data) => console.log(data),
+    },
+  );
+
+  useEffect(() => {
+    if (inView) {
+      fetchNextPage();
+    }
+  }, [inView]);
+
   return (
     <>
       <PageTitle title="커뮤니티" />
-      <BtnAdd
-        link={'posts'}
-        text="게시글 작성"
-        community={true}
-      />
+      <BtnAdd link={'posts'} text="게시글 작성" community={true} />
       <CommunityColor>
         <CommunityContainer>
           <Header title="커뮤니티" link="/community" />
           <Category>
-            {/* arr.filter((elm => elm.category === '카테고리명')) */}
             <h3>전체</h3>
             {communityRoles.map((v) => (
               <h3 key={v}>{v}</h3>
@@ -41,10 +73,18 @@ const Community = () => {
           <h5>인기순</h5>
         </Order>
 
-        <ShortCard />
-        <ShortCard />
+        {postList &&
+          postList.pages.map((page, index) => (
+            <React.Fragment key={index}>
+              {page.data.map((post) => (
+                <ShortCard key={post.postId} post={post} />
+              ))}
+            </React.Fragment>
+          ))}
       </CommunityColor>
+      {isFetchingNextPage ? <Loading /> : <div ref={ref} />}
     </>
   );
 };
+
 export default Community;
