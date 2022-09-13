2b32a2827c552d0050b90329cb7403b45869d1a0
slozche <ldhfj2141@gmail.com>
2022-09-13 15:28:30 +0900

    :bug: 가족코드 페이지 라디오 선택 불가 수정

--- src/styles/Label.js
+++ src/styles/Label.js
@@ -1,42 +1,42 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 export const Label = styled.label`
-    font-size: 14px;
-    font-weight: 600;
-    color: #191919;
-`
+  font-size: 14px;
+  font-weight: 600;
+  color: #191919;
+`;
 
 export const RadioLabel = styled.label`
-    display: inline-block;
-    cursor: pointer;
-    width: 50%;
-    height: 52px;
-    border: 1px solid #DDDDDD;
-    margin-top: 8px;
-    background: #FFFFFF;
-    font-size: 13px;
-    line-height: 52px;
-    color: #7D7D7D;
-    text-align: center;
-`
+  display: inline-block;
+  cursor: pointer;
+  width: 50%;
+  height: 52px;
+  border: 1px solid #dddddd;
+  margin-top: 8px;
+  background: #ffffff;
+  font-size: 13px;
+  line-height: 52px;
+  color: #7d7d7d;
+  text-align: center;
+`;
 
 export const CodeLabel = styled.label`
-    height: 130px;
-    border-radius: 12px;
-    padding: 42px 0 0 30px;
-    cursor: pointer;
-`
+  height: 130px;
+  border-radius: 12px;
+  padding: 42px 0 0 30px;
+  cursor: pointer;
+`;
 
 export const RoleLabel = styled.label`
-    height: 107px;
-    padding-top: 60px;
-    box-shadow: 2px 2px 20px rgba(184, 187, 192, 0.4);
-    border-radius: 5px;
-    display: block;
-    font-size: 20px;
-    font-weight: 500;
-    line-height: 24px;
-    color: #191919;
-    text-align: center;    
-    cursor: pointer;
-`
+  height: 107px;
+  padding-top: 60px;
+  box-shadow: 2px 2px 20px rgba(184, 187, 192, 0.4);
+  border-radius: 5px;
+  display: block;
+  font-size: 20px;
+  font-weight: 500;
+  line-height: 24px;
+  color: #191919;
+  text-align: center;
+  cursor: pointer;
+`;
